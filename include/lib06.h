d6a8511421faf9ddbe504644634ae0bc60f58b75
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-05-10 00:32:05 +0100

    added platfrom and visibility directives

--- include/lib06.h
+++ include/lib06.h
@@ -17,17 +17,33 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-/* ASSUMING THE PROJECT IS BEING BUILT WITH WINDOWS */ 
-/* ACCESS THE DESIGANTED CALLING CONVENTION */ 
-
-#if defined(_WIN32)
+#if defined _WIN32
     #ifdef LIB_06_EXPORT
        #define LIB_06_MAIN DECLSPEC (dllexport)
     #else
        #define LIB_06_MAIN DECLSPEC (dllimport)
     #endif
 
-#define LIB_06_MAIN
+#elif defined _GNUC && _GNUC >= 4
+    #define LIB_06_MAIN __attribute__ ((visibility)("default"))
+#else
+    #define LIB_06_MAIN
+#endif
+
+#ifndef LIB_TYPES
+#define LIB_TYPES
+
+typedef unsigned char BYTE;
+typedef signed char SIGNED_BYTE;
+typedef unsigned short SHORT;
+
+#define LIB_06_WIN          (0)
+#define LIB_06_OSX          (1)
+#define LIB_06_XENIA        (2)
+#define LIB_06_RPCS3        (3)
+
+#define LIB_06_UNK          NULL
+
 #endif
 
 #endif
