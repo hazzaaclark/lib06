e1563994b9d45d673ec7ed6b0257224df1c2e3ce
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-05-12 16:19:27 +0100

    Add files via upload

---
+++ include/decomp/engine/maths/math_util.h
@@ -0,0 +1,19 @@
+/* Copyright (C) 2023 Harry Clark */
+
+/* SONIC THE HEDGEHOG (2006) Library */
+
+/* THIS FILE PERTAINS TO THE MATHS UTILITY OF THE ENGINE DECOMP OF */
+/* SONIC THE HEDGEHOG (2006) */
+
+#ifndef MATH_UTIL
+#define MATH_UTIL
+
+/* COMMON MATHS TYPES */
+
+#define PI               3.141592653589793
+#define SECOND           1
+#define METER            100
+#define KILOMETER        (METER * 1000)
+#define GRAVITY          (METER * 9.8) / (SECOND * SECOND)
+
+#endif
