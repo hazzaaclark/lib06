cf7bf4c3eb592552088d3701a64baee0252d9d9e
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-05-12 16:58:25 +0100

    Add files via upload

---
+++ include/decomp/engine/maths/math_util.cpp
@@ -0,0 +1,15 @@
+/* Copyright (C) 2023 Harry Clark */
+
+/* SONIC THE HEDGEHOG (2006) Library */
+
+/* THIS FILE PERTAINS TO THE MATHS UTILITY OF THE ENGINE DECOMP OF */
+/* SONIC THE HEDGEHOG (2006) */
+
+/* NESTED INCLUDES */
+
+#include "math_util.h"
+
+/* SYSTEM INCLUDES */
+
+#include <math.h>
+
