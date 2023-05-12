064f485e99ed4e1ca1f0eaf41007532ba650fac8
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-05-12 16:42:16 +0100

    added trig functions

--- include/decomp/engine/maths/math_util.h
+++ include/decomp/engine/maths/math_util.h
@@ -8,6 +8,10 @@
 #ifndef MATH_UTIL
 #define MATH_UTIL
 
+/* NESTED INCLUDES */
+
+#include "common.h"
+
 #if defined (COMMON_MATHS)
 #define COMMON_MATHS
 #else
@@ -19,6 +23,15 @@
 #define KILOMETER        (METER * 1000)
 #define GRAVITY          (METER * 9.8) / (SECOND * SECOND)
 
+#define SIN (VALUE) SINE_TABLE[(U16) (VALUE) >> 3]
+#define COS (VALUE) COSINE_TABLE[(U16) (VALUE >> 3)]
+
 #endif
+
+extern "C"
+{
+	extern F32 SINE_TABLE[];
+	extern F32 COSINE_TABLE[];
+}
 
 #endif
