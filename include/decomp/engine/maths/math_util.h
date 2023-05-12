a394b3e83ecffe50737108f8a157076209ec2298
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-05-12 16:32:11 +0100

    Update math_util.h

--- include/decomp/engine/maths/math_util.h
+++ include/decomp/engine/maths/math_util.h
@@ -8,12 +8,17 @@
 #ifndef MATH_UTIL
 #define MATH_UTIL
 
-/* COMMON MATHS TYPES */
+#if defined (COMMON_MATHS)
+#define COMMON_MATHS
+#else
+#define COMMON_MATHS
 
 #define PI               3.141592653589793
 #define SECOND           1
 #define METER            100
 #define KILOMETER        (METER * 1000)
 #define GRAVITY          (METER * 9.8) / (SECOND * SECOND)
+
+#endif
 
 #endif
