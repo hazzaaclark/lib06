c9c2486e124f302a0b301430942e3fa9a41de0dc
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-05-14 16:27:05 +0100

    Update lib06.h

--- include/lib06.h
+++ include/lib06.h
@@ -18,24 +18,29 @@
 #include <stdbool.h>
 
 #if defined _WIN32
-    #ifdef LIB_06_EXPORT
-       #define LIB_06_MAIN DECLSPEC (dllexport)
-    #else
-       #define LIB_06_MAIN DECLSPEC (dllimport)
-    #endif
-
-#elif defined _GNUC && _GNUC >= 4
-    #define LIB_06_MAIN __attribute__ ((visibility)("default"))
+#ifdef LIB_06_EXPORT
+#define LIB_06_MAIN DECLSPEC (dllexport)
 #else
-    #define LIB_06_MAIN
+#define LIB_06_MAIN DECLSPEC (dllimport)
 #endif
 
-#ifndef LIB_TYPES
+#elif defined _GNUC && _GNUC >= 4
+#define LIB_06_MAIN __attribute__ ((visibility)("default"))
+#else
+#define LIB_06_MAIN
+#endif
+
+#if defined (LIB_TYPES)
+#define LIB_TYPES
+#else
 #define LIB_TYPES
 
 typedef unsigned char BYTE;
 typedef signed char SIGNED_BYTE;
 typedef unsigned short SHORT;
+
+#define LIB_06_MIN (VALUE_A, VALUE_B) ((VALUE_A) < VALUE_B) ? (VALUE_A) : (VALUE_B))
+#define LIB_6_MAX ()
 
 #define LIB_06_WIN          (0)
 #define LIB_06_OSX          (1)
